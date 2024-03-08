1df4c471ad48a02d062b848375456311eb40cced
Abhay <abhaykanoji@gmail.com>
2024-03-08 21:55:27 +0530

    counter complete with assignmanet

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -4,30 +4,24 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  let [count,setcount] = useState(1)
+
+  let addup=()=>{
+    if(count<20){
+      setcount(count +1)
+    }
+  }
+  let adddown = ()=>{
+    if(count>0){
+      setcount(count -1)
+  }
+}
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>counter : {count}</h1>
+      <button onClick={addup}>increase</button>
+      <button onClick={adddown}>decrease</button>
     </>
   )
 }
